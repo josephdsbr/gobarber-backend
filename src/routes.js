8521ba5c7e053e403cdba0d56895622439cb34fe
José Vinícius <josephdsbr@gmail.com>
2019-10-22 20:59:00 -0300

    adicionando alterações em routes

--- src/routes.js
+++ src/routes.js
@@ -2,13 +2,20 @@ import { Router } from 'express';
 import UserController from './app/controllers/UserController';
 import SessionController from './app/controllers/SessionController';
 import authMiddleware from './app/middlewares/auth';
+import multer from 'multer';
+import multerConfig from './config/multer';
 
 const routes = new Router();
+const upload = multer(multerConfig);
 
 routes.post('/users', UserController.Store);
 routes.post('/sessions', SessionController.Store);
 
 routes.use(authMiddleware);
 routes.put('/users', UserController.Update);
+
+routes.post('/files', upload.single('file'), (req, res) => {
+  return res.json({ ok: true})
+})
 
 module.exports = routes;
