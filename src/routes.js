92cb81deeb3387cee759e72cb5f3702eec414e1b
jose.vinicius <josephdsbr@gmail.com>
2019-10-25 13:57:22 -0300

    dando robustes aos CRUD, fazendo integração ao MongoDB e fazendo processo de notificação

--- src/routes.js
+++ src/routes.js
@@ -5,8 +5,10 @@ import FileController from './app/controllers/FileController';
 import ProviderController from './app/controllers/ProviderController';
 import SessionController from './app/controllers/SessionController';
 import AppointmentController from './app/controllers/AppointmentController';
+import NotificationController from './app/controllers/NotificationController';
 import authMiddleware from './app/middlewares/auth';
 import multerConfig from './config/multer';
+import ScheduleController from './app/controllers/ScheduleController';
 
 const routes = new Router();
 const upload = multer(multerConfig);
@@ -19,7 +21,15 @@ routes.use(authMiddleware);
 routes.put('/users', UserController.update);
 
 routes.post('/appointments', AppointmentController.store);
+routes.get('/appointments', AppointmentController.index);
+routes.delete('/appointments/:id', AppointmentController.delete);
+
 routes.get('/providers', ProviderController.index);
+
+routes.get('/notifications', NotificationController.index);
+routes.put('/notifications/:id', NotificationController.update);
+
+routes.get('/schedule', ScheduleController.index);
 routes.post('/files', upload.single('file'), FileController.store);
 
 module.exports = routes;
