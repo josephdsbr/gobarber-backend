6e30f872326f74e9d8f68080cdfbd442cf4455f8
josephdsbr <josephdsbr@gmail.com>
2019-11-21 00:00:42 -0300

    fazendo algumas alteracoes para funcionar o Schedule controller

--- src/app/controllers/ScheduleController.js
+++ src/app/controllers/ScheduleController.js
@@ -28,13 +28,6 @@ class ScheduleController {
           [Op.between]: [startOfDay(parsedDate), endOfDay(parsedDate)],
         },
       },
-      include: [
-        {
-          model: 'User',
-          as: 'user',
-          attributes: ['name'],
-        },
-      ],
       order: ['date'],
     });
     return res.json(appointments);
